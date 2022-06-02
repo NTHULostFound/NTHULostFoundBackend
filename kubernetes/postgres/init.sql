9c9ffc4927cfa3a6a44d77b6e190a05db28b19fa
Yi Kuo <kuokuoyiyi@gmail.com>
2022-06-02 14:20:09 +0800

    feat(DB): create databse schema

---
+++ kubernetes/postgres/init.sql
@@ -0,0 +1,33 @@
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE ItemType AS ENUM ('FOUND', 'LOST');
+
+CREATE TABLE IF NOT EXISTS items (
+    "uuid" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    "type" ItemType NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "date" TIMESTAMP WITH TIME ZONE NOT NULL,
+    "place" TEXT NOT NULL,
+    "how" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
+    "resolved" BOOLEAN NOT NULL DEFAULT FALSE,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS images (
+    "uuid" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    "itemId" UUID REFERENCES items(uuid) ON DELETE CASCADE NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    "uuid" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    "fcmToken" TEXT NOT NULL,
+    "name" TEXT,
+    "studentId" TEXT,
+    "email" TEXT,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
